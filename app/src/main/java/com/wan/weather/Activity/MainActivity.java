1159741417a6bcfff96c13bdc7cd4f9266b5751d
Stars-One <Stars-One@users.noreply.github.com>
2019-05-13 13:02:20 +0800

    First commit

---
+++ app/src/main/java/com/wan/weather/Activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.wan.weather.Activity;
+
+import android.os.Bundle;
+
+import com.wan.weather.BaseActivity;
+import com.wan.weather.R;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
